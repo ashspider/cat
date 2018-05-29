3f1a95017f2a441333f2bc184d0d551f6a2300b7
ashspider <39323954+ashspider@users.noreply.github.com>
2018-05-29 20:25:04 +0530

    Update connect.py

--- web_app/connect.py
+++ web_app/connect.py
@@ -1,5 +1,8 @@
+import os,requests
 from flask import Flask, render_template, url_for, request, session, redirect
-from flask.ext.pymongo import PyMongo
+from flask_pymongo import PyMongo
+from flask import make_response
+from flask_cors import CORS, cross_origin
 import bcrypt
 
 app = Flask(__name__)
